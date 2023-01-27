884e3aec80cbd22d8e45e559f4a8de2ff0e0e8e7
Angela4200 <78118177+Angela4200@users.noreply.github.com>
2023-01-26 20:47:22 -0600

    Add Module 2 Folder
    Module 2 assignment

--- Module_1/app.js
+++ Module_1/app.js
@@ -1,3 +1,6 @@
 console.log("What is the best way to keep your coding languages organized?") ;
 
 document.write("Digital Media Programming Course 2023");
+
+
+
---
+++ Module_2/app.js
@@ -0,0 +1,25 @@
+// New way of coding for JavaScript
+
+// Create a global variable
+let life = 100;
+
+life = life - 50;
+
+// String and global variable
+let name = "life" ;
+
+// Create an object variable using const and global variable
+const question = {type: "module 2" , model:"2351" , color:"purple"} ;
+
+// Aray and global variable
+let cars = ["BMW", "Ford", "Ram"] ;
+
+function myfunction() { // block variable starts here.
+    document.getElementById("line").innerHTML="Is this right?" ;
+    document.getElementById("test").innerHTML="I am not sure?" ;
+}   // block variable ends here.
+
+myfunction();
+
+console.log(life);
+
