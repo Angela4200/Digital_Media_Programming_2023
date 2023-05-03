a9f9354546a3b03ae8e7affbdff2dcc332ed4191
Angela4200 <78118177+Angela4200@users.noreply.github.com>
2023-05-03 13:28:49 -0500

    Module_11
    Adding falling ball game

--- Module_11/app.js
+++ Module_11/app.js
@@ -107,6 +107,43 @@ function restartGame(){
 }
 // Tic Tac Toe Ends Here
 
+//Falling Ball Game Starts Here
+var character = document.getElementById("character");
+var interval;
+var both = 0;
+
+function moveLeft(){
+    var left =
+    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left>0){
+        character.style.left = left - 2 + "px";
+    }
+}
+function moveRight(){
+    var left =
+    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left>380){
+        character.style.left = left + 2 + "px";
+    }
+}
+
+document.addEventListener("keydown", event =>{
+    if(both==0){
+        both++;
+        if(event.key==="ArrowLeft"){
+            interval = setInterval(moveLeft, 1);
+        }
+        if(event.key==="ArrowRight"){
+            interval = setInterval(moveRight, 1);
+        }
+    }
+});
+
+document.addEventListener("keyup", event =>{
+    clearInterval(interval);
+    both=0;
+});
+
 
 // Tabs Start here
 //function openCity(evt, cityName) {
